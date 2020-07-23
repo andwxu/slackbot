f1d2c9d367f3f042a85ce6787c5b35a2db0063a9
Andrew Xu <andwxu@gmail.com>
2020-07-23 15:21:51 -0400

    Pairing system done

--- index.js
+++ index.js
@@ -9,6 +9,49 @@ const channelID = 'C0189AEUN8Y'; //replace this later with the channelID you wan
 
 (async() => {
     try {
+        const result = await web.conversations.members({
+            channel: channelID,
+        });
+        let members = result.members;
+        let pairedMembers = [];
+        
+        // Random pairing sequence
+        let i = 0; // current index of pairedMembers
+        while (members.length > 1) {
+            let pair1 = members.splice(Math.floor(Math.random() * members.length), 1);
+            let pair2 = members.splice(Math.floor(Math.random() * members.length), 1);
+            pairedMembers[i] = { pair1, pair2 };
+        }
+
+        
+        // Sending DM's to paired members
+        for (let j = 0; j < pairedMembers.length; j++) {
+            // Opening DM with the two paired members
+            let convResult = await web.conversations.open({
+                users: `${pairedMembers[j].pair1},${pairedMembers[j].pair2}`
+            });
+            let convID = convResult.channel.id;
+            
+            // Sending opening message
+            let messageResult = await web.chat.postMessage({
+                channel: convID,
+                text: `You have been paired up! Get to know each other`
+            });
+            
+        }
+        
+
+    } catch(error) {
+        console.log(error.data);
+    }
+})();
+
+
+
+
+/*
+(async() => {
+    try {
         const result = await web.chat.postMessage({
             text: 'testing now haha',
             channel: channelID,
@@ -19,3 +62,4 @@ const channelID = 'C0189AEUN8Y'; //replace this later with the channelID you wan
         console.log(error.data);
     }
 })();
+*/
