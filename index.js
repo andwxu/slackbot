3feb3408ad6e37ab958d7c537cd13751ed0461d2
Andrew Xu <andwxu@gmail.com>
2020-07-23 15:46:00 -0400

    Pairing system every week done

--- index.js
+++ index.js
@@ -7,59 +7,42 @@ const web = new WebClient(process.env.BOT_TOKEN)
 // Channel ID
 const channelID = 'C0189AEUN8Y'; //replace this later with the channelID you want on the VSA slack
 
-(async() => {
-    try {
-        const result = await web.conversations.members({
-            channel: channelID,
-        });
-        let members = result.members;
-        let pairedMembers = [];
-        
-        // Random pairing sequence
-        let i = 0; // current index of pairedMembers
-        while (members.length > 1) {
-            let pair1 = members.splice(Math.floor(Math.random() * members.length), 1);
-            let pair2 = members.splice(Math.floor(Math.random() * members.length), 1);
-            pairedMembers[i] = { pair1, pair2 };
-        }
-
-        
-        // Sending DM's to paired members
-        for (let j = 0; j < pairedMembers.length; j++) {
-            // Opening DM with the two paired members
-            let convResult = await web.conversations.open({
-                users: `${pairedMembers[j].pair1},${pairedMembers[j].pair2}`
+// Executes the pairing every 30 seconds, can change this to a week later
+setInterval(
+    (async() => {
+        try {
+            const result = await web.conversations.members({
+                channel: channelID,
             });
-            let convID = convResult.channel.id;
+            let members = result.members;
+            let pairedMembers = [];
             
-            // Sending opening message
-            let messageResult = await web.chat.postMessage({
-                channel: convID,
-                text: `You have been paired up! Get to know each other`
-            });
+            // Random pairing sequence
+            let i = 0; // current index of pairedMembers
+            while (members.length > 1) {
+                let pair1 = members.splice(Math.floor(Math.random() * members.length), 1);
+                let pair2 = members.splice(Math.floor(Math.random() * members.length), 1);
+                pairedMembers[i] = { pair1, pair2 };
+            }
             
+            // Sending DM's to paired members
+            for (let j = 0; j < pairedMembers.length; j++) {
+                // Opening DM with the two paired members
+                let convResult = await web.conversations.open({
+                    users: `${pairedMembers[j].pair1},${pairedMembers[j].pair2}`
+                });
+                let convID = convResult.channel.id;
+                
+                // Sending opening message
+                let messageResult = await web.chat.postMessage({
+                    channel: convID,
+                    text: `You have been paired up! Get to know each other`
+                });
+                
+            }
+
+        } catch(error) {
+            console.log(error.data);
         }
-        
-
-    } catch(error) {
-        console.log(error.data);
-    }
-})();
-
-
-
-
-/*
-(async() => {
-    try {
-        const result = await web.chat.postMessage({
-            text: 'testing now haha',
-            channel: channelID,
-        });
-
-        console.log(`Sent the ${result.ts} in conversation ${channelID}`);
-    } catch (error) {
-        console.log(error.data);
-    }
-})();
-*/
+    })
+, 30000);
